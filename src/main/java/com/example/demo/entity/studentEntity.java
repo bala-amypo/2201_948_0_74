ff75b0138e2fa41f1ff461a670e5f28571f12e0b
bala-amypo <bala@amypo.in>
2025-12-17 08:35:56 +0000

    User commit

--- src/main/java/com/example/demo/entity/studentEntity.java
+++ src/main/java/com/example/demo/entity/studentEntity.java
@@ -1,3 +1,16 @@
 package com.example.demo.entity;
 
-public 
+@Entity
+public class studentEntity{
+
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    private long id;
+    
+    @
+    private String name;
+    
+    @Email(message="Email Format is not Valid")
+
+
+}
